ef22790d44c39e0975dc54505b8be675d38bf3f6
shiva508 <dasarishiva1@gmail.com>
2023-12-17 18:22:21 +0530

    first commit

---
+++ src/main/java/com/comrade/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.comrade.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome message";
+    }
+}
---
+++ src/test/java/com/comrade/JenkinsComradeIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsComradeIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
